66d8e382186c16c54e164a06782a6809a183c68c
Ceeram <c33ram@gmail.com>
2012-05-10 16:26:27 +0200

    config method now also can return value for string type parameter

--- Lib/Pdf/Engine/AbstractPdfEngine.php
+++ Lib/Pdf/Engine/AbstractPdfEngine.php
@@ -13,12 +13,17 @@ Abstract class AbstractPdfEngine {
 /**
  * Set the config
  *
- * @param array $config
- * @return array Returns config array
+ * @param mixed $config Null, string or array. Pass array of configs to set.
+ * @return mixed Returns Returns config value if $config is string, else returns config array.
  */
 	public function config($config = null) {
 		if (is_array($config)) {
 			$this->_config = $config;
+		} elseif (is_string($config)) {
+			if (!empty($this->_config[$config])) {
+				return $this->_config[$config];
+			}
+			return false;
 		}
 		return $this->_config;
 	}
