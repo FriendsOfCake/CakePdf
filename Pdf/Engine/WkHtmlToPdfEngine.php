04a6efcb009879ed776af35997f060f38b04612c
Jelle Henkens <jelle.henkens@gmail.com>
2012-05-11 16:16:43 +0100

    Adding title support

--- Pdf/Engine/WkHtmlToPdfEngine.php
+++ Pdf/Engine/WkHtmlToPdfEngine.php
@@ -96,6 +96,10 @@ class WkHtmlToPdfEngine extends AbstractPdfEngine {
 			}
 		}
 
+		$title = $this->_Pdf->title();
+		if ($title !== null) {
+			$command .= sprintf(' --title %s', escapeshellarg($title));
+		}
 		$command .= " - -";
 
 		return $command;
