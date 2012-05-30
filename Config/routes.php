0f3df19ab87c2ad3838242b879b9d7afae623014
Ceeram <c33ram@gmail.com>
2012-05-30 21:36:04 +0200

    using new setExtensions method

--- Config/routes.php
+++ Config/routes.php
@@ -1,3 +1,7 @@
 <?php
-	$extensions = array_merge(array('pdf'), Router::extensions());
-	call_user_func_array('Router::parseExtensions', $extensions);
+/**
+ * Note: Router::setExtensions() is available since CakePHP 2.2
+ * If you use 2.1 dont load this routes file, and add pdf to your parseExtensions() in app/Config/routes.php
+ */
+Router::parseExtensions();
+Router::setExtensions('pdf');
