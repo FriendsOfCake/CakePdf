df31b8de14ca8e700ad34db6459701ee1060fca9
Walther Lalk <walther@octoplus.co.za>
2013-04-16 10:52:10 +0200

    Fix error if options are not an array

--- Pdf/Engine/WkHtmlToPdfEngine.php
+++ Pdf/Engine/WkHtmlToPdfEngine.php
@@ -112,7 +112,11 @@ class WkHtmlToPdfEngine extends AbstractPdfEngine {
 				$options['margin-' . $key] = $value . 'mm';
 			}
 		}
-		$options = array_merge($options, $this->config('options'));
+
+		$configOptions = $this->config('options');
+		if(is_array($configOptions)) {
+			$options = array_merge($options, $configOptions);
+		}
 
 		$command = $this->binary;
 		foreach ($options as $key => $value) {
