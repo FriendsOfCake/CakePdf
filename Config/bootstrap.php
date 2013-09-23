d167f8732c8bfed669101242897924a099a599cc
Ceeram <c33ram@gmail.com>
2013-09-23 16:13:52 +0200

    remove bootstrap file

--- Config/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * This file is kept just for backwards compatibility. The recommended way is to
- * use the RequestHandler's view class mapping feature as shown in README instead
- * of loading this bootstrap file.
- */
-App::uses('PdfView', 'CakePdf.View');
