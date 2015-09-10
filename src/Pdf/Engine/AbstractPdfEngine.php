8960bbb87899f52de559202a48671d8120ee6633
Florian Krämer <florian.kraemer@cakedc.com>
2015-09-10 13:04:56 +0200

    Changing AbstractPdfEngine to use InstanceConfigTrait

--- src/Pdf/Engine/AbstractPdfEngine.php
+++ src/Pdf/Engine/AbstractPdfEngine.php
@@ -2,10 +2,15 @@
 
 namespace CakePdf\Pdf\Engine;
 
+use Cake\Core\InstanceConfigTrait;
 use CakePdf\Pdf\CakePdf;
 
 abstract class AbstractPdfEngine
 {
+
+    use InstanceConfigTrait;
+
+    protected $_defaultConfig = [];
 
     /**
      * Instance of CakePdf class
@@ -13,13 +18,6 @@ abstract class AbstractPdfEngine
      * @var CakePdf
      */
     protected $_Pdf = null;
-
-    /**
-     * Configurations
-     *
-     * @var array
-     */
-    protected $_config = [];
 
     /**
      * Constructor
@@ -37,23 +35,4 @@ abstract class AbstractPdfEngine
      * @return string
      */
     abstract public function output();
-
-    /**
-     * Set the config
-     *
-     * @param mixed $config Null, string or array. Pass array of configs to set.
-     * @return mixed Returns Returns config value if $config is string, else returns config array.
-     */
-    public function config($config = null)
-    {
-        if (is_array($config)) {
-            $this->_config = $config;
-        } elseif (is_string($config)) {
-            if (!empty($this->_config[$config])) {
-                return $this->_config[$config];
-            }
-            return false;
-        }
-        return $this->_config;
-    }
 }
