ce88ac886ce2e4ecf7df9454af54167c7eb0981c
Florian Krämer <florian.kraemer@cakedc.com>
2015-12-20 23:45:27 +0100

    Adding test file, forgot it.

---
+++ tests/TestCase/Pdf/Engine/TexToPdfEngineTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace CakePdf\Test\TestCase\Pdf\Engine;
+
+use CakePdf\Pdf\CakePdf;
+use CakePdf\Pdf\Engine\WkHtmlToPdfEngine;
+use Cake\TestSuite\TestCase;
+
+/**
+ * TexToPdfEngineTest class
+ *
+ * @package       CakePdf.Test.Case.Pdf.Engine
+ */
+class TexToPdfEngineTest extends TestCase
+{
+
+    /**
+     * Tests that the engine generates the right command
+     */
+    public function testGetCommand()
+    {
+        $class = new \ReflectionClass('CakePdf\Pdf\Engine\TexToPdfEngine');
+        $method = $class->getMethod('_getCommand');
+        $method->setAccessible(true);
+
+        $Pdf = new CakePdf([
+            'engine' => [
+                'className' => 'CakePdf.TexToPdf',
+            ]
+        ]);
+
+        $result = $method->invokeArgs($Pdf->engine(), []);
+        $expected = '/usr/bin/latexpdf --output-directory "' . TMP . 'pdf"';
+        $this->assertEquals($expected, $result);
+    }
+}
