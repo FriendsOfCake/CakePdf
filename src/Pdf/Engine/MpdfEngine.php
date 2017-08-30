26e5b1b1926b4bbc1185096fe7906b40b091b531
Mikkel Bonde Christensen <mikbonde@gmail.com>
2017-08-30 12:17:38 +0200

    Support Mpdf 7.0 with support for PHP 7.x

--- src/Pdf/Engine/MpdfEngine.php
+++ src/Pdf/Engine/MpdfEngine.php
@@ -14,9 +14,21 @@ class MpdfEngine extends AbstractPdfEngine
         //mPDF often produces a whole bunch of errors, although there is a pdf created when debug = 0
         //Configure::write('debug', 0);
         $orientation = $this->_Pdf->orientation() == 'landscape' ? 'L' : 'P';
-        $MPDF = new \mPDF($this->_Pdf->encoding(), $this->_Pdf->pageSize() . '-' . $orientation);
-        $MPDF->writeHTML($this->_Pdf->html());
 
-        return $MPDF->Output('', 'S');
+        if (class_exists('\mpdf')) {
+            //Mpdf < 7.0
+            $mpdf = new \mPDF($this->_Pdf->encoding(), $this->_Pdf->pageSize() . '-' . $orientation);
+        } else {
+            //Mpdf >= 7.0
+            $mpdf = new \Mpdf\Mpdf([
+                'mode' => $this->_Pdf->encoding(),
+                'format' => $this->_Pdf->pageSize(),
+                'orientation' => $orientation,
+            ]);
+        }
+
+        $mpdf->writeHtml($this->_Pdf->html());
+
+        return $mpdf->Output('', 'S');
     }
 }
