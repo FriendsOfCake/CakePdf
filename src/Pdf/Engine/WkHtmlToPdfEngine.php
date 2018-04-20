64757dfa1e84e8633e4a8341a82467d42bd6b0ae
Paulo Alexandre Chaves Pinto <paulo.alexandre88@gmail.com>
2018-04-20 15:19:26 -0400

    Update WkHtmlToPdfEngine.php
    Fix changes in default windows binary

--- src/Pdf/Engine/WkHtmlToPdfEngine.php
+++ src/Pdf/Engine/WkHtmlToPdfEngine.php
@@ -33,7 +33,7 @@ class WkHtmlToPdfEngine extends AbstractPdfEngine
         $this->_windowsEnvironment = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
         
         if ( $this->_windowsEnvironment ){
-            $this->$_binary = 'C:/Progra~1/wkhtmltopdf/bin/wkhtmltopdf.exe';
+            $this->_binary = 'C:/Progra~1/wkhtmltopdf/bin/wkhtmltopdf.exe';
         }
     }
 
