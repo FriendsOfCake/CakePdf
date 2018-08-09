2d72839d9cda7b30ab88ccc1986931e52e19653c
Jan Ptacek <jan.ptacek@gmail.com>
2018-08-09 11:11:00 +0200

    dompdf from composer for 1.0 branch
    current detection for dompdf installed via composer does not work, I do not see how to fix it, so I opt for an autoloaded version all the time
    
    honor the passthrough options as well

--- Pdf/Engine/DomPdfEngine.php
+++ Pdf/Engine/DomPdfEngine.php
@@ -1,6 +1,9 @@
 <?php
+require APP . 'Vendor/autoload.php';
+
+use Dompdf\Dompdf;
+
 App::uses('AbstractPdfEngine', 'CakePdf.Pdf/Engine');
-App::uses('Multibyte', 'I18n');
 
 class DomPdfEngine extends AbstractPdfEngine {
 
@@ -11,17 +14,6 @@ class DomPdfEngine extends AbstractPdfEngine {
  */
 	public function __construct(CakePdf $Pdf) {
 		parent::__construct($Pdf);
-		if (!defined('DOMPDF_FONT_CACHE')) {
-			define('DOMPDF_FONT_CACHE', TMP);
-		}
-		if (!defined('DOMPDF_TEMP_DIR')) {
-			define('DOMPDF_TEMP_DIR', TMP);
-		}
-
-		if (App::import('Vendor', 'DomPDF', array('file' => 'dompdf' . DS . 'dompdf' . DS . 'dompdf_config.inc.php'))) {
-			return;
-		}
-		App::import('Vendor', 'CakePdf.DomPDF', array('file' => 'dompdf' . DS . 'dompdf_config.inc.php'));
 	}
 
 /**
@@ -30,7 +22,13 @@ class DomPdfEngine extends AbstractPdfEngine {
  * @return string raw pdf data
  */
 	public function output() {
-		$DomPDF = new DOMPDF();
+		$DomPDF = new Dompdf();
+		
+		$options = $this->config('options');
+		if ($options) foreach ($this->config('options') as $option => $value) {
+			$DomPDF->set_option($option, $value);
+		}
+		
 		$DomPDF->set_paper($this->_Pdf->pageSize(), $this->_Pdf->orientation());
 		$DomPDF->load_html($this->_Pdf->html());
 		$DomPDF->render();
