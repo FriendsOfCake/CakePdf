c31f56d39e1c2b234aefba52704171eb467e6dde
Rafael Queiroz <rafaelfqf@gmail.com>
2020-01-29 19:37:37 -0300

    Using shell_exec('which') instead is_executable on binary validation

--- src/Pdf/Engine/WkHtmlToPdfEngine.php
+++ src/Pdf/Engine/WkHtmlToPdfEngine.php
@@ -106,7 +106,7 @@ class WkHtmlToPdfEngine extends AbstractPdfEngine
         if ($binary) {
             $this->_binary = $binary;
         }
-        if (!is_executable($this->_binary)) {
+        if (!shell_exec("which {$this->_binary}")) {
             throw new Exception(sprintf('wkhtmltopdf binary is not found or not executable: %s', $this->_binary));
         }
 
